c2150c94af8fff8bbcb65b6f6e82582b838cf26f
sebastianboga <sebastian.boga@endava.com>
2020-08-02 10:36:46 +0300

    fixing syntax error and adding test cases to validate the add -> company functionality

--- api/company/add/index.php
+++ api/company/add/index.php
@@ -5,7 +5,7 @@
 //  cui   - cod fiscal ; contine RO daca este platitor de TVA
 //  registru - numar de inregistrare in registrul comertului
 
-
+//  cui este obligatoriu, ceilalti paramatrii fiind optionali
 
 include_once("../../../util/index.php");
 
@@ -13,20 +13,22 @@ $id = guid();
 
 
 
-if (isset($_POST('companie')))
-  if (!empty($_POST('companie')))
-	  $denumire = $_POST('companie');
+if (isset($_POST['companie']))
+  if (!empty($_POST['companie']))
+	  $denumire = $_POST['companie'];
+
+if (isset($_POST['registru']))
+  if (!empty($_POST['registru']))
+	  $registru = $_POST['registru'];
+
+if (isset($_POST['cui']))
+{if (!empty($_POST['cui']))
+  {	  $cui = $_POST['cui'];
 
 
 
-if (isset($_POST('cui')))
-  if (!empty($_POST('cui')))
-	  $cui = $_POST('cui');
-
-
-if (isset($_POST('registru')))
-  if (!empty($_POST('registru')))
-	  $registru = $_POST('registru');
+  
+  
 
 $sql = "INSERT INTO company (id,denumire,cui,registru) VALUES ('$id','$denumire','$cui','$registru')";
 
@@ -35,4 +37,7 @@ if (mysqli_query($concompany, $sql)) {
 } else {
   echo "error : " . mysqli_error($concompany);
 }
+}}
+   else 
+   { http_response_code(409); echo 'cui este obligatoriu';}
 ?>
