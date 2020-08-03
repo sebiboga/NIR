c8d79793b303be11a23f184ba414db029bd5dc47
sebastianboga <sebastian.boga@endava.com>
2020-08-03 16:48:22 +0300

    get company id

---
+++ api/company/getcompanyid/index.php
@@ -0,0 +1,37 @@
+<?php
+
+// GET parameters:
+//  cui   - cod fiscal ; contine RO daca este platitor de TVA
+
+//  cui este obligatoriu
+
+
+include_once("../../../util/index.php");
+
+$id = guid();
+
+
+
+
+
+if (isset($_GET['cui']))
+{if (!empty($_GET['cui']))
+  {	  $cui = $_GET['cui'];
+
+
+
+$sql = "SELECT id FROM company WHERE cui = '$cui' ";
+
+
+if ($result = mysqli_query($concompany, $sql)) {
+	$row = mysqli_fetch_assoc($result);
+    $id  = $row["id"];
+  echo "{id:".$id."}";
+} else {
+  echo "error : " . mysqli_error($concompany);
+  http_response_code(404);
+}
+}}
+   else 
+   { http_response_code(200); echo 'not found';}
+?>
