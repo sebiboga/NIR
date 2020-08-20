4ed58c7de9ecf4b06cb46de36bcdfb2df363f38e
sebastianboga <sebastian.boga@endava.com>
2020-08-20 19:10:40 +0300

    add getcompanyname

---
+++ api/company/getcompanyname/index.php
@@ -0,0 +1,37 @@
+<?php
+
+// GET parameters:
+//  cui   - cod fiscal ; contine RO daca este platitor de TVA
+
+//  cui este obligatoriu
+
+
+include_once("../../../util/index.php");
+
+$id = guid();
+
+
+
+
+
+if (isset($_GET['cui']))
+{if (!empty($_GET['cui']))
+  {	  $cui = $_GET['cui'];
+
+
+
+$sql = "SELECT denumire FROM company WHERE cui = '$cui' ";
+
+
+if ($result = mysqli_query($concompany, $sql)) {
+	$row = mysqli_fetch_assoc($result);
+    $name  = $row["denumire"];
+  echo '{"companie":"'.$name.'"}';
+} else {
+  echo "error : " . mysqli_error($concompany);
+  http_response_code(404);
+}
+}}
+   else 
+   { http_response_code(200); echo 'not found';}
+?>
