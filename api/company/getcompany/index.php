a1c5111eeefb36d78e3da79038b357df5c135122
MB <MB@MB-PC>
2020-10-02 20:18:49 +0300

    added getcompany

---
+++ api/company/getcompany/index.php
@@ -0,0 +1,39 @@
+<?php
+
+// GET parameters:
+//  cui   - cod fiscal ; contine RO daca este platitor de TVA
+
+//  cui este obligatoriu
+
+
+include_once("../../../util/index.php");
+
+$id = guid();
+
+
+
+
+
+if (isset($_GET['cui']))
+{if (!empty($_GET['cui']))
+  {	  $cui = $_GET['cui'];
+
+
+
+$sql = "SELECT * FROM company WHERE cui = '$cui' ";
+
+
+if ($result = mysqli_query($concompany, $sql)) {
+	$row = mysqli_fetch_assoc($result);
+    $name  = $row["denumire"];
+	$cui = $row["cui"];
+  echo '{"companie":"'.$name.'",
+  "cui":"'.$cui.'"}';
+} else {
+  echo "error : " . mysqli_error($concompany);
+  http_response_code(404);
+}
+}}
+   else 
+   { http_response_code(200); echo 'not found';}
+?>
