43d24d3ffa575d5ebc0f382b147573d0b0c3bb1c
sebastianboga <sebastian.boga@endava.com>
2020-10-08 21:03:22 +0300

    proof of concept

---
+++ POC/sql.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS csvtest
+CREATE TABLE employeeinfo(
+emp_id VARCHAR(50)  PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date VARCHAR(50)
+)
